136b0be22442f43d980203289ab5b828bea92953
R <b_ro_m@list.ru>
2023-01-31 13:03:06 +0300

    1

--- lecture_snippets/Probe_003.py
+++ lecture_snippets/Probe_003.py
@@ -1,17 +1,69 @@
-a, b = 1, 2
-(a, b) = (1, 2)
-
-for a, b in [(1, 2), (3, 4), (5, 6,)] :
-    print(a + b)
-
-# for (a, b) in [(1, 2), (3, 4)] :
-#     print(a + b)
+# zoo_pets = [
+#     'lion', 'skunk',
+#     'elephant', 'horse',
+# ]
 #
-pair_list = [(1, 2), (3, 4), (5, 6)]
+# for i, animal in enumerate(zoo_pets):
+#     print(i, animal)
+#
+# zoo_pets = [
+#      'lion', 'skunk',
+#      'elephant', 'horse',
+#     ]
+# for animal in zoo_pets :
+#      print(animal)
+#      del zoo_pets[0]
+# print(zoo_pets)
 
-for a, b in pair_list :
-    print(a + b)
+# for i in range(100, 600, 50):
+#     print(i)
 
-# triple_list = [(1, 2, 3), (4, 5, 6)]
-# for a, b, c in triple_list :
-#     print(a, b, c)
+# for i in range(12, 76, 3):
+#     print(i)
+#
+# for i in range(10):
+#     print(i)
+
+# animal = 'lion'
+# for char in animal :
+#     print(char)
+#
+# zoo_pets = [
+#     'lion', 'skunk',
+#     'elephant', 'horse',
+# ]
+# for animal in zoo_pets:
+#     for char in animal:
+#         print(char)
+#     print(animal)
+
+# zoo_pet_mass = {
+#     'lion': 300,
+#     'skunk': 5,
+#     'elephant': 5000,
+#     'horse': 400,
+# }
+# total_mass = 0
+# for animal in zoo_pet_mass:
+#     print(animal, zoo_pet_mass[animal])
+#     total_mass += zoo_pet_mass[animal]
+# print('Общая масса животных', total_mass)
+#
+# total_mass = 0
+# for animal, mass in zoo_pet_mass.items():
+#     print(animal, mass)
+#     total_mass += mass
+# print('Общая масса животных', total_mass)
+#
+# total_mass = 0
+# for mass in zoo_pet_mass.values():
+#     print(mass)
+#     total_mass += mass
+# print('Общая масса животных', total_mass)
+
+# values - только значения
+# items - только вес
+
+
+def some_func():
+    print('Привет! Я функция')
