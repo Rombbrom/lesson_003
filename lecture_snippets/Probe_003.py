cea5a7903260acef2c3cb3b82a5f1eb92be1f002
R <b_ro_m@list.ru>
2023-01-31 10:36:14 +0300

    1

--- lecture_snippets/Probe_003.py
+++ lecture_snippets/Probe_003.py
@@ -1,6 +1,17 @@
-print('дратути!')
-i = 1
-while i < 10:
-    i = i * 2
-    print(i)
-print('дотвидания!')
+a, b = 1, 2
+(a, b) = (1, 2)
+
+for a, b in [(1, 2), (3, 4), (5, 6,)] :
+    print(a + b)
+
+# for (a, b) in [(1, 2), (3, 4)] :
+#     print(a + b)
+#
+pair_list = [(1, 2), (3, 4), (5, 6)]
+
+for a, b in pair_list :
+    print(a + b)
+
+# triple_list = [(1, 2, 3), (4, 5, 6)]
+# for a, b, c in triple_list :
+#     print(a, b, c)
